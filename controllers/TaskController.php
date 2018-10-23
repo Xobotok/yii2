f24f8757ce3df9b5e864743e6e11cc43617c159b
Xobotok <meshcheryakov.m.i@yandex.ru>
2018-10-23 14:58:42 +0500

    Initial commit

---
+++ controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xobat
+ * Date: 23.10.2018
+ * Time: 14:51
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class TaskController extends Controller {
+
+    public function actionIndex() {
+        return 'Hello World';
+    }
+
+}
---
+++ models/Task.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xobat
+ * Date: 23.10.2018
+ * Time: 14:53
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Task extends ActiveRecord {
+
+}
